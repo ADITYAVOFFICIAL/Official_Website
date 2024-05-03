679463a4df7fcccaa886c46e1a8fe89f7b20aa29
ADITYA VERMA <adityav1304@gmail.com>
2024-05-03 15:00:37 +0530

    added quest button

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -92,6 +92,7 @@ function Footer() {
                   &copy; {isMobile ? "2024" : "2024 Made with ❤️ by"}
                   <NavLink to="#"> SIGKDD Student Chapter SRM KTR </NavLink>
                 </p>
+                <NavLink to="https://srmsigkddquest.vercel.app" className="btn quest-btn">WILDCARD RECRUITMENT</NavLink>
               </div>
             </div>
           </div>
